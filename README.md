534ab78ee668c3372688c380a328b42dfe4dc2d8
Casey Yelland <cyelland@blizzard.com>
2019-03-27 16:52:07 -0700

    Rename script, update readme

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # ffxiv
 Assorted FFXIV tools
+
+## raid_comp_score_calc
+
+Attempts to calculate group composition score. Uses data manually sourced from fflogs for Omega: Alphascape (Savage). [Link](https://www.fflogs.com/zone/statistics/25/#dataset=95&class=Any)
+2 weeks of data, set to top 5% parses
+and a community sourced data sheet that shows "Raid DPS Gain" stats per job. [Link](https://docs.google.com/spreadsheets/d/1aSzKYCsE4_DUdDcxbxrRR272JECmETw3Hoq644ZWUZM/edit#gid=1564032043)
+
+Score and DPS are equivalent on fflogs. Score is just DPS normalized across 0-100. If my group buffs yield a
+"Raid DPS Gain" of 1% and the PLD's score is 50 then I modify the PLD's score by 1% resulting in 50.5. Adjusted group
+comp score is achieved by applying this across all group members.
+
+###### Assumptions:
+- Tanks, Healers, and the 4th DPS's (always a BRD) buffs are left out since their buffs are constant.
+- BRD's are also very hard to calculate "Raid DPS Gain" since Foe Requiem is mana based and toggled
+- MCH is left out because LOOOL reroll BRD
+- Certain buffs only apply to 'others' (MNK Brotherhood) but actually include self for simplicity
+- Single target buffs are always on and don't check for a valid target (e.g. DRG Dragon Sight)
+- Decent chance that SAM/BLM numbers are higher than other jobs since AST's generally throw the single target
+cards their way
---
+++ raid_comp_score_calc.py
@@ -0,0 +1,141 @@
+# raid_comp_score_calc.py
+#
+# Attempts to calculate group composition score. Uses data from jobs.json
+import argparse
+import copy
+import json
+import random
+import sys
+import itertools
+
+
+class Job(object):
+    def __init__(self, name, json_obj):
+        self.name = name
+        self.base_score = json_obj["score"]
+        self.tank = json_obj["tank"] if "tank" in json_obj else False
+        self.healer = json_obj["healer"] if "healer" in json_obj else False
+        self.melee = json_obj["melee"] if "melee" in json_obj else False
+        self.ranged = json_obj["ranged"] if "ranged" in json_obj else False
+        self.physical_damage_percent = json_obj["physical"] if "physical" in json_obj else 0.0
+        self.magical_damage_percent = json_obj["magical"] if "magical" in json_obj else 0.0
+        self.damage_buff_all = 0.0
+        self.damage_buff_physical = 0.0
+        self.damage_buff_magical = 0.0
+        self.damage_buff_single = 0.0
+        self.score = self.base_score
+        if "damage_buff" in json_obj:
+            damage_buff_obj = json_obj["damage_buff"]
+            if "all" in damage_buff_obj:
+                self.damage_buff_all = damage_buff_obj["all"]
+            if "physical" in damage_buff_obj:
+                self.damage_buff_physical = damage_buff_obj["physical"]
+            if "magical" in damage_buff_obj:
+                self.damage_buff_magical = damage_buff_obj["magical"]
+            if "single" in damage_buff_obj:
+                self.damage_buff_single = damage_buff_obj["single"]
+
+
+class CompOutput(object):
+    def __init__(self, jobs, base_score, adjusted_score):
+        self.jobs = jobs
+        self.name = ' '.join(x.name for x in jobs)
+        self.base_score = base_score
+        self.adjusted_score = adjusted_score
+
+    def print_results(self):
+        print '{0}: adjusted:{1:.2f} base:{2:.2f}'.format(self.name, self.adjusted_score, self.base_score)
+
+
+def load_job_data():
+    with open("jobs.json", "r") as read_file:
+        return json.load(read_file)
+    return None
+
+
+def calc_group_score(dps_jobs, non_dps_jobs):
+    # print "Running calc for group comp: {0} {1} {2}".format(dps_jobs[0].name, dps_jobs[1].name, dps_jobs[2].name)
+
+    total_base_score = 0.0
+    total_adjusted_score = 0.0
+    full_group = copy.deepcopy(dps_jobs) + copy.deepcopy(non_dps_jobs)
+    # for each member in the group
+    for n in range(len(full_group)):
+        this_job = full_group[n]
+
+        # increase this jobs score by the bonuses from other group members (includes self, bit hacky)
+        for other_job in full_group:
+            if other_job.damage_buff_all != 0.0:
+                this_job.score += (other_job.damage_buff_all / 100.0) * this_job.base_score
+            if other_job.damage_buff_physical > 0.0 and this_job.physical_damage_percent > 0.0:
+                this_job.score += ((other_job.damage_buff_physical / 100.0) * this_job.base_score) * (this_job.physical_damage_percent / 100.0)
+            if other_job.damage_buff_magical > 0.0 and this_job.magical_damage_percent > 0.0:
+                this_job.score += ((other_job.damage_buff_magical / 100.0) * this_job.base_score) * (this_job.magical_damage_percent / 100.0)
+
+        # this seems weird, but if we have any single target score increases just apply it to ourselves for simplicity
+        if this_job.damage_buff_single:
+            this_job.score += (this_job.damage_buff_single / 100.0) * this_job.base_score
+
+        total_base_score += this_job.base_score
+        total_adjusted_score += this_job.score
+    return total_base_score, total_adjusted_score
+
+
+def is_dps_job(job_name):
+    return job_name != 'PLD' and job_name != 'WAR' and job_name != 'SCH' and job_name != 'AST'
+
+
+def comp_filter(comp):
+    # do not allow all phys damage
+    # do not allow all magical damage
+    # RDM doesn't apply
+    phys_count = len(list(filter(lambda job: job.name != 'RDM' and job.physical_damage_percent > 0.0, comp)))
+    mag_count = len(list(filter(lambda job: job.name != 'RDM' and job.magical_damage_percent > 0.0, comp)))
+    return mag_count < 3 and phys_count < 3
+
+
+def main():
+    parser = argparse.ArgumentParser(prog="raid_comp_score_calc",
+                                     description='Calculate highest raid comp score for FFXIV', add_help=True)
+    parser.add_argument('--fixed_comp', type=str, help='CSV list of jobs to use',
+                        default='')
+    parser.add_argument('--filter', type=int, help='Turn job filter on/off. Filters out all phys or all magic comps',
+                        default='1')
+
+    args = parser.parse_args()
+
+    job_data = load_job_data()
+    if not job_data:
+        raise RuntimeError('Unable to load job data.')
+
+    # Gather the DPS jobs only
+    dps_job_keys = [x for x in job_data.keys() if is_dps_job(x)]
+
+    if args.fixed_comp == '':
+        # create all combinations of 3 DPSers ('ABCD', 2) => AA AB AC AD BB BC BD CC CD DD
+        # we exclude the 4th DPS since that will always be reserved for a BRD/MCH and their calculations are too complex
+        combinations = [list(x) for x in itertools.combinations_with_replacement(dps_job_keys, 3)]
+        print "{0} group comp combinations generated".format(len(combinations))
+    else:
+        combinations = [args.fixed_comp.split(',')]
+    # convert to Job objs
+    combinations = [[Job(y, job_data[y]) for y in x] for x in combinations]
+    if args.filter == 1:
+        # certain comps are deemed "dumb", filter those out
+        combinations = [x for x in combinations if comp_filter(x)]
+
+    non_dps_jobs = [Job(x, job_data[x]) for x in job_data.keys() if not is_dps_job(x)]
+
+    output = []
+    for comp in combinations:
+        total_base_score, total_adjusted_score = calc_group_score(comp, non_dps_jobs)
+        output.append(CompOutput(comp, total_base_score, total_adjusted_score))
+
+    output.sort(key=lambda i: i.adjusted_score)
+    for o in output:
+        o.print_results()
+
+
+if __name__ == "__main__":
+    if not main():
+        sys.exit(1)
